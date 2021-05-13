b777b7eef37765cc634f808555fdbf82be7b932a
ZYROUGE <zyrouge@hotmail.com>
2021-05-13 15:56:27 +0530

    Added exports in index fi;e

---
+++ lib/index.ts
@@ -0,0 +1,11 @@
+import FourAnime from "./extractors/4anime";
+import Gogoanime from "./extractors/gogoanime";
+import Simplydotmoe from "./extractors/simplydotmoe";
+
+export default {
+    extractors: {
+        FourAnime,
+        Gogoanime,
+        Simplydotmoe,
+    },
+};
