ed372df1ece0e31565d64dd447eb840043d4fe7d
ZYROUGE <zyrouge@hotmail.com>
2021-05-14 09:36:28 +0530

    Fixed sources (removed yourupload, mp4upload as no way of scraping jwplayer without puppeteer)

--- lib/extractors/sources/index.ts
+++ lib/extractors/sources/index.ts
@@ -1,7 +1,8 @@
 import GogoplayStream from "./gogoplay-streaming";
 import GogoplayLoad from "./gogoplay-load";
+import { SourceRetriever } from "./model";
 
-export const extractors = [GogoplayStream, GogoplayLoad];
+export const extractors: SourceRetriever[] = [GogoplayStream, GogoplayLoad];
 
 export const getExtractor = (url: string) => {
     return extractors.find((ext) => ext.validate(url));
