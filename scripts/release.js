441d4490a74a4651886f6dad088b1c02f7762fc6
ZYROUGE <zyrouge@hotmail.com>
2021-05-17 09:59:06 +0530

    1.0.6

--- scripts/release.js
+++ scripts/release.js
@@ -1,40 +1,37 @@
-const child_process = require("child_process");
-const util = require("util");
+const { execSync: exec } = require("child_process");
 
 const log = (tag, txt) => console.log(`[${tag}] ${txt}`);
 
-const exec = util.promisify(child_process.exec);
-
-const build = async () => {
-    const { stdout } = await exec("npm run build");
+const build = () => {
+    const { stdout } = exec("npm run build");
     log("ts-build", "Build success");
     log("ts-build", stdout);
 };
 
-const version = async () => {
+const version = () => {
     if (process.argv.includes("--no-version")) {
         log("version", "Skipping version due to '--no-version' arg!");
         return;
     }
 
-    const { stdout } = await exec("npm version patch");
+    const { stdout } = exec("npm version patch");
     log("version", `Version updated to ${stdout.trim()}!`);
 };
 
-const git = async () => {
+const git = () => {
     if (process.argv.includes("--no-git")) {
         log("Skipping git due to '--no-git' arg!");
         return;
     }
 
     {
-        const { stdout } = await exec("git add .");
+        const { stdout } = exec("git add .");
         log("git", "Added files to git!");
         log("git", stdout);
     }
 
     {
-        const { stdout } = await exec(
+        const { stdout } = exec(
             `git commit -m "${require("../package.json").version}"`
         );
         log("git", "Committed files to git!");
@@ -42,47 +39,47 @@ const git = async () => {
     }
 };
 
-const push = async () => {
+const push = () => {
     if (process.argv.includes("--no-push")) {
         log("Skipping push due to '--no-push' arg!");
         return;
     }
 
-    const { stdout } = await exec("git push");
+    const { stdout } = exec("git push");
     log("git", "Pushed files to git!");
     log("git", stdout);
 };
 
-const merge = async () => {
+const merge = () => {
     if (process.argv.includes("--no-merge")) {
         log("Skipping merge due to '--no-merge' arg!");
         return;
     }
 
     {
-        const { stdout } = await exec("git checkout main");
+        const { stdout } = exec("git checkout main");
         log("git", "Switched to main branch!");
         log("git", stdout);
     }
 
-    await git();
-    await push();
+    git();
+    push();
 
     {
-        const { stdout } = await exec("git checkout next");
+        const { stdout } = exec("git checkout next");
         log("git", "Switched back to next branch!");
         log("git", stdout);
     }
 };
 
-const start = async () => {
+const start = () => {
     const start = Date.now();
 
-    await build();
-    await git();
-    await version();
-    await push();
-    await merge();
+    build();
+    git();
+    version();
+    push();
+    merge();
 
     log("release-it", `Successfully completed in ${Date.now() - start}ms!`);
 };
